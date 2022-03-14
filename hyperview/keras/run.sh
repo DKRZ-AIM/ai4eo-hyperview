735eac8b3ff530a4038c2de8dcbf7b070c072914
Kuzu, Ridvan Salih <ridvan.kuzu@dlr.de>
2022-03-15 00:57:35 +0100

    init commit ridvan

--- hyperview/keras/run.sh
+++ hyperview/keras/run.sh
@@ -2,7 +2,7 @@
 module use $OTHERSTAGES
 module load Stages/2020
 module load TensorFlow/2.5.0-Python-3.8.5
-srun --exclusive --nodes=1 --gres=gpu:4  --time=11:45:00 --account=hai_cons_ee python main.py -m 1 -l 0.0100 -b 16 --num-epochs 120 --train-dir 'train_data/train_data/' --label-dir 'train_data/train_gt.csv' --eval-dir 'test_data/' --out-dir 'modeldir/' &
+srun --exclusive --nodes=1 --gres=gpu:4  --time=12:00:00 --account=hai_cons_ee python main.py -m 1 -l 0.0100 -b 16 --num-epochs 120 --train-dir 'train_data/train_data/' --label-dir 'train_data/train_gt.csv' --eval-dir 'test_data/' --out-dir 'modeldir/' &
 srun --exclusive --nodes=1 --gres=gpu:4  --time=11:45:00 --account=hai_cons_ee python main.py -m 1 -l 0.0010 -b 16 --num-epochs 120 --train-dir 'train_data/train_data/' --label-dir 'train_data/train_gt.csv' --eval-dir 'test_data/' --out-dir 'modeldir/' &
 srun --exclusive --nodes=1 --gres=gpu:4  --time=11:45:00 --account=hai_cons_ee python main.py -m 1 -l 0.1000 -b 16 --num-epochs 120 --train-dir 'train_data/train_data/' --label-dir 'train_data/train_gt.csv' --eval-dir 'test_data/' --out-dir 'modeldir/' &
 srun --exclusive --nodes=1 --gres=gpu:4  --time=11:45:00 --account=hai_cons_ee python main.py -m 2 -l 0.0100 -b 16 --num-epochs 120 --train-dir 'train_data/train_data/' --label-dir 'train_data/train_gt.csv' --eval-dir 'test_data/' --out-dir 'modeldir/' &
