6042bbd6e69dbb200e695d8c3434e9299b0ca1d1
Kuzu, Ridvan Salih <ridvan.kuzu@dlr.de>
2022-04-01 17:34:42 +0200

    test-ridvan

--- hyperview/keras/main.py
+++ hyperview/keras/main.py
@@ -49,7 +49,7 @@ def main():
         os.makedirs(args.out_dir)
     image_shape = (args.width, args.width)
     dataset = DataGenerator(args.train_dir, args.label_dir, args.eval_dir,
-                            valid_size=0.24,
+                            valid_size=0.20,
                             image_shape=image_shape,
                             batch_size=args.batch_size)
 
