08ef47c2ef46f1d6b67dd1a6a113e8a036476740
Frauke Albrecht <k202143@tc8login.novalocal>
2022-04-06 14:23:39 +0200

    submit job script

---
+++ hyperview/random_forest/jobs/run.sh
@@ -0,0 +1,16 @@
+echo 'HELLO BOX'
+codedir=/work/frauke/ai4eo-hyperview/hyperview/random_forest
+datadir=/work/shared_data/2022-ai4eo_hyperview
+conda init
+source ~/.bashrc
+conda activate ai4eo_hyper
+echo "conda env activated"
+
+echo $codedir
+cd $codedir
+
+PYTHONPATH=$PYTHONPATH:"$codedir"
+export PYTHONPATH
+
+python3 rf_train.py --in-data $datadir --submission-dir $codedir/submissions --debug
+
